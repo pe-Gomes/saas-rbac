7574bd49d7412e19b33c838e6c5379490039cffa
Pedro Gomes <pcgbernardes@gmail.com>
2024-05-09 21:02:38 -0400

    feat: Add Prisma and Base DB Schema

---
+++ apps/api/start-database.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+DB_CONTAINER_NAME="saas-rbac"
+
+if ! [ -x "$(command -v docker)" ]; then
+  echo -e "Docker is not installed. Please install docker and try again.\nDocker install guide: https://docs.docker.com/engine/install/"
+  exit 1
+fi
+
+if [ "$(docker ps -q -f name=$DB_CONTAINER_NAME)" ]; then
+  echo "Database container '$DB_CONTAINER_NAME' already running"
+  exit 0
+fi
+
+if [ "$(docker ps -q -a -f name=$DB_CONTAINER_NAME)" ]; then
+  docker start "$DB_CONTAINER_NAME"
+  echo "Existing database container '$DB_CONTAINER_NAME' started"
+  exit 0
+fi
+
+# import env variables from .env
+set -a
+source .env
+
+DB_PASSWORD=$(echo "$DATABASE_URL" | awk -F':' '{print $3}' | awk -F'@' '{print $1}')
+DB_USER=$(echo "$DATABASE_URL" | awk -F'://' '{print $2}' | awk -F ':' '{print $1}')
+
+if [ "$DB_PASSWORD" = "password" ]; then
+  echo "You are using the default database password"
+  read -p "Should we generate a random password for you? [y/N]: " -r REPLY
+  if ! [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo "Please set a password in the .env file and try again"
+    exit 1
+  fi
+  # Generate a random URL-safe password
+  DB_PASSWORD=$(openssl rand -base64 12 | tr '+/' '-_')
+  sed -i -e "s#:password@#:$DB_PASSWORD@#" .env
+fi
+
+echo "$DATABASE_URL"
+echo "$DB_PASSWORD"
+echo "$DB_USER"
+
+docker run -d \
+  --name $DB_CONTAINER_NAME \
+  -e POSTGRES_USER="$DB_USER" \
+  -e POSTGRES_PASSWORD="$DB_PASSWORD" \
+  -e POSTGRES_DB="$DB_CONTAINER_NAME" \
+  -p 5432:5432 \
+  docker.io/postgres:16-alpine && echo "Database container '$DB_CONTAINER_NAME' was successfully created"
